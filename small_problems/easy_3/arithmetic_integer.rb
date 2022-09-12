f2ab644f1fe6b973e2cc62b43c71a7e77876237f
Zach Kane <zkane756@icloud.com>
2022-09-11 21:59:13 -0600

    "First File"

---
+++ small_problems/easy_3/arithmetic_integer.rb
@@ -0,0 +1,36 @@
+
+
+puts "Please Enter Number One"
+a = gets.chomp.to_i
+
+puts "Please Enter Number Two"
+b = gets.chomp.to_i
+
+puts "Added They Are"
+p a+b
+puts "Subtracted They Are"
+p a-b
+puts "The Product Is"
+p a*b
+puts "The Quotient Is"
+p a/b
+puts "The Remainder Is"
+p a%b
+puts "The Power Is"
+p a**b
+
+# def prompt(message)
+#   puts "==> #{message}"
+# end
+
+# prompt("Enter the first number:")
+# first_number = gets.chomp.to_i
+# prompt("Enter the second number:")
+# second_number = gets.chomp.to_i
+
+# prompt("#{first_number} + #{second_number} = #{first_number + second_number}")
+# prompt("#{first_number} - #{second_number} = #{first_number - second_number}")
+# prompt("#{first_number} * #{second_number} = #{first_number * second_number}")
+# prompt("#{first_number} / #{second_number} = #{first_number / second_number}")
+# prompt("#{first_number} % #{second_number} = #{first_number % second_number}")
+# prompt("#{first_number} ** #{second_number} = #{first_number**second_number}")
